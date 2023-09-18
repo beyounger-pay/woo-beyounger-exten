eb5be6c092313f6ca8305d7dd5bfaf085de27423
lilonghui <1249738273@qq.com>
2023-09-18 19:18:18 +0800

    拼接memo

--- inst-payments-gateway/includes/controllers/ByPaymentController.php
+++ inst-payments-gateway/includes/controllers/ByPaymentController.php
@@ -74,7 +74,7 @@ class ByPaymentController {
         $home_url = home_url();
         $home_url = rtrim(str_replace('https://','',str_replace('http://','',$home_url)));
         preg_match('@^(?:https://)?([^/]+)@i',str_replace('www.','',$home_url), $matches);
-        $memo = $matches[1] . '-' . $order->get_id() . "\n";
+        $memo = $matches[1] . '-' . $order->get_id();
         echo '拼接memo:' . $memo ."\n";
 
 
